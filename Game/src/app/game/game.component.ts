23888c31e79471159488366e3f4e882e5c8f282e
Jimmy <jimmyl98@hotmail.com>
2023-02-22 02:50:36 +0100

    Made background and player character design

--- Game/src/app/game/game.component.ts
+++ Game/src/app/game/game.component.ts
@@ -1,35 +1,42 @@
-import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import kaboom from 'kaboom';
 import LevelMap from '../level-map';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.scss']
+  styleUrls: ['./game.component.scss'],
 })
 export class GameComponent implements AfterViewInit, OnDestroy {
-  @ViewChild("mycanvas")
+  @ViewChild('mycanvas')
   private canvas = {} as ElementRef;
   private levelMap = new LevelMap();
-  private player: any
-  private fixedmap: any
-  private camera: any
+  private player: any;
+  private playerBody: any;
+  private fixedmap: any;
+  private camera: any;
   private k: any;
 
   ngOnDestroy(): void {
-    console.log("hehdestroy")
+    console.log('hehdestroy');
   }
 
   ngAfterViewInit(): void {
-    this.initGame()
+    this.initGame();
     this.initPlayer();
     this.initLevelCollisions();
     this.initInputController();
 
     // Create camera
     this.camera = this.player.pos;
-    this.k.camScale(4)
-    this.render()
+    this.render();
   }
 
   initGame() {
@@ -38,65 +45,69 @@ export class GameComponent implements AfterViewInit, OnDestroy {
       global: false,
       width: 1500,
       height: 800,
-      font: "sinko",
+      font: 'sinko',
       canvas: this.canvas.nativeElement,
-      background: [0, 0, 0,],
-    })
-    this.k.cam
+      background: [0, 0, 0],
+    });
+    this.k.cam;
     // Load sprites and assets
     this.k.loadBean();
-    this.k.loadSprite("map", "/assets/Rbygg.png");
-    this.k.loadSprite("avatar","/assets/spritesheet.png", {
-			// The image contains 25 frames layed out horizontally, slice it into individual frames
-			sliceX: 25,
-			// Define animations
-			anims: {
-				idle: {
-					// Starts from frame 0, ends at frame 10
-					from: 0,
-					to: 10,
-					// Frame per second
-					speed: 10,
-					loop: true,
-				},
-				run: {
-					from: 11,
-					to: 22,
-					speed: 15,
-					loop: true,
-				},
-			},
-		});
+    this.k.loadSprite('map', '/assets/Rbygghighres.png');
+    this.k.loadSprite('avatar', '/assets/spriteShadowDarker777.png', {
+      // The image contains 25 frames layed out horizontally, slice it into individual frames
+      sliceX: 9,
+      // Define animations
+      anims: {
+        idle: {
+          // Starts from frame 0, ends at frame 10
+          from: 0,
+          to: 0,
+          // Frame per second
+          speed: 10,
+          loop: true,
+        },
+        run: {
+          from: 1,
+          to: 8,
+          speed: 11,
+          loop: true,
+        },
+      },
+    });
   }
 
   initPlayer() {
     const k = this.k;
+    k.debug.inspect = true;
     // Create player
-    this.fixedmap = k.add([
-      k.sprite("map"),
-      k.pos(-512,-512),
-    ])
+    this.fixedmap = k.add([k.sprite('map'), k.pos(-512, -512)]);
     this.player = k.add([
-      k.sprite("avatar"),   // sprite() component makes it render as a sprite
-      k.pos(120, 80),     // pos() component gives it position, also enables movement
-      k.origin("center"), // origin() component defines the pivot point (defaults to "topleft")
-      k.area(),
-      k.scale(0.6),
-      k.solid(),
+      k.sprite('avatar'), // sprite() component makes it render as a sprite
+      k.origin('center'), // origin() component defines the pivot point (defaults to "topleft")
+      k.pos(120, 80), // pos() component gives it position, also enables movement
       {
         movement: {
           left: false,
           right: false,
           up: false,
-          down: false
+          down: false,
         },
         facing: 0,
-        speed: 100,
+        speed: 400,
       },
-    ])
+    ]);
+    this.playerBody = k.add([
+      k.rect(80,110),
+      k.origin('center'),
+      k.pos(120, 90),
+      k.follow(this.player, k.vec2(0,5)),
+      k.opacity(0),
+      k.area(),
+      k.solid(),
+    ]);
 
     // Player loop
-    this.player.play("idle");
+    this.player.play('idle');
     this.player.onUpdate(() => {
       // Update player
       let speed = this.player.speed;
@@ -104,26 +115,29 @@ export class GameComponent implements AfterViewInit, OnDestroy {
         (v) => v
       ).length;
       if (movement > 1) speed *= 0.71;
-      if (movement > 0 && this.player.curAnim() !== "run") this.player.play("run");
-      else if (movement == 0 && this.player.curAnim() !== "idle") this.player.play("idle");
-      if (this.player.movement.left){
+      if (movement > 0 && this.player.curAnim() !== 'run')
+        this.player.play('run');
+      else if (movement == 0 && this.player.curAnim() !== 'idle')
+        this.player.play('idle');
+      if (this.player.movement.left) {
         this.player.move(-speed, 0);
         this.player.flipX(true);
-      } 
-        
+      }
+
       if (this.player.movement.right) {
         this.player.move(speed, 0);
         this.player.flipX(false);
-      } 
+      }
       if (this.player.movement.up) this.player.move(0, -speed);
       if (this.player.movement.down) this.player.move(0, speed);
+     
       // .angle is a property provided by rotate() component, here we're incrementing the angle by 120 degrees per second, dt() is the time elapsed since last frame in seconds
-      this.player.angle += 120 * k.dt()
-    })
+      this.player.angle += 120 * k.dt();
+    });
   }
 
   initLevelCollisions() {
-    const k = this.k;
+    /* const k = this.k;
     // Generate level map collisions
     for (let i = 0; i < this.levelMap.realfagsbygget.length; i++) {
       const rect = this.levelMap.realfagsbygget[i];
@@ -134,35 +148,35 @@ export class GameComponent implements AfterViewInit, OnDestroy {
         k.outline(1),
         k.area(),
         k.solid(),
-      ])
-    }
+      ]);
+    } */
   }
 
   initInputController() {
     // Controles / user input
-    window.addEventListener("keydown", (event) => {
+    window.addEventListener('keydown', (event) => {
       const { key } = event;
-      if (key === "ArrowLeft" || key === "a") {
+      if (key === 'ArrowLeft' || key === 'a') {
         this.player.movement.left = true;
         this.player.facing = 0;
-      } else if (key === "ArrowUp" || key === "w") {
+      } else if (key === 'ArrowUp' || key === 'w') {
         this.player.movement.up = true;
-      } else if (key === "ArrowRight" || key === "d") {
+      } else if (key === 'ArrowRight' || key === 'd') {
         this.player.movement.right = true;
         this.player.facing = 1;
-      } else if (key === "ArrowDown" || key === "s") {
+      } else if (key === 'ArrowDown' || key === 's') {
         this.player.movement.down = true;
       }
     });
-    window.addEventListener("keyup", (event) => {
+    window.addEventListener('keyup', (event) => {
       const { key } = event;
-      if (key === "ArrowLeft" || key === "a") {
+      if (key === 'ArrowLeft' || key === 'a') {
         this.player.movement.left = false;
-      } else if (key === "ArrowUp" || key === "w") {
+      } else if (key === 'ArrowUp' || key === 'w') {
         this.player.movement.up = false;
-      } else if (key === "ArrowRight" || key === "d") {
+      } else if (key === 'ArrowRight' || key === 'd') {
         this.player.movement.right = false;
-      } else if (key === "ArrowDown" || key === "s") {
+      } else if (key === 'ArrowDown' || key === 's') {
         this.player.movement.down = false;
       }
     });
@@ -172,12 +186,11 @@ export class GameComponent implements AfterViewInit, OnDestroy {
     const k = this.k;
     // Game loop
     k.onUpdate(() => {
-			this.camera = k.vec2(
-				k.lerp(this.camera.x, this.player.pos.x, 0.03),
-				k.lerp(this.camera.y, this.player.pos.y, 0.03)
-			);
+      this.camera = k.vec2(
+        k.lerp(this.camera.x, this.player.pos.x, 0.03),
+        k.lerp(this.camera.y, this.player.pos.y, 0.03)
+      );
       k.camPos(this.camera);
-		});
+    });
   }
-
 }
