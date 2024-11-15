70f2e551c3f7e73d9721b9683069e028eca123fd
Jimmy Luong <jimmy.luong@norconsult.com>
2024-11-15 12:42:42 +0100

    Update to Angular 18

--- Game/src/app/app.module.ts
+++ Game/src/app/app.module.ts
@@ -11,27 +11,23 @@ import { ConvertToSpacePipe } from './shared/convert-to-space.pipe';
 import { ConvertToDoubleSpacePipe } from './shared/convert-to-double-space.pipe';
 import { LobbyComponent } from './lobby/lobby.component';
 import { HighscoreComponent } from './highscore/highscore.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { ConvertMsToHhmmssPipe } from './shared/convert-ms-to-hhmmss.pipe';
 import { ProgressbarComponent } from './progressbar/progressbar.component';
 import { NotebookComponent } from './notebook/notebook.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    GameComponent,
-    ModalComponent,
-    ConvertToBlankPipe,
-    ConvertToSpacePipe,
-    ConvertToDoubleSpacePipe,
-    ConvertMsToHhmmssPipe,
-    LobbyComponent,
-    HighscoreComponent,
-    ProgressbarComponent,
-    NotebookComponent,
-  ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
-  providers: [],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [
+        AppComponent,
+        GameComponent,
+        ModalComponent,
+        ConvertToBlankPipe,
+        ConvertToSpacePipe,
+        ConvertToDoubleSpacePipe,
+        ConvertMsToHhmmssPipe,
+        LobbyComponent,
+        HighscoreComponent,
+        ProgressbarComponent,
+        NotebookComponent,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule, AppRoutingModule, FormsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {}
